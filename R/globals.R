2b1bff55f04a1cf8e459e18f775ca78dd5c6ee0f
ShashikadzeB <bachuki.shashikadze@gmail.com>
2023-01-30 11:59:39 +0100

    globals

---
+++ R/globals.R
@@ -0,0 +1,7 @@
+utils::globalVariables(c('unique_id', 'Bioreplicate', 'Intensity', 'n_valid', 'n_ko', 'n_pep', '.',
+                  'files', 'size', 'Q.Value', 'Global.Q.Value', 'Global.PG.Q.Value', 'Lib.Q.Value',
+                  'Lib.PG.Q.Value', 'Proteotypic','Quantity.Quality','Run','Stripped.Sequence', 'Precursor.Quantity',
+                  'Precursor.Normalised', 'Modified.Sequence', 'Modification', 'Precursor.Charge', 'Potential.contaminant',
+                  'Only.identified.by.site',
+                  'Reverse', 'unique_id', 'Condition', 'prot.id', 'log2FC', 'p.val', 'p.adj','l2fc', 'significant',
+                  'unique_id', 'accession', 'n_ko', 'n_imputed', 'n_pep'))
